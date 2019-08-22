77229ac843c049db11215ca08084993a2810b13a
页颂教育 <yesongedu@sina.com>
2019-08-22 18:00:05 +0800

    first16

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,9 @@
 jQuery.noConflict();
 jQuery(document).ready(function($){
-								
-							
+
+
 function lightboxPhoto() {
-	
+
 	jQuery("a[data-gal^='prettyPhoto']").prettyPhoto({
 			animationSpeed:'fast',
 			slideshow:5000,
@@ -11,27 +11,27 @@ function lightboxPhoto() {
 			show_title:false,
 			overlay_gallery: false
 		});
-	
+
 	}
-	
+
 		if(jQuery().prettyPhoto) {
-	
-		lightboxPhoto(); 
-			
+
+		lightboxPhoto();
+
 	}
-	
-	
+
+
 if (jQuery().quicksand) {
 
  	// Clone applications to get a second collection
 	var $data = $(".portfolio-area").clone();
-	
+
 	//NOTE: Only filter on the main portfolio page, not on the subcategory pages
 	$('.portfolio-categ li').click(function(e) {
-		$(".filter li").removeClass("active");	
+		$(".filter li").removeClass("active");
 		// Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
 		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
-		
+
 		if (filterClass == 'all') {
 			var $filteredData = $data.find('.portfolio-item2');
 		} else {
@@ -43,11 +43,11 @@ if (jQuery().quicksand) {
 		}, function () {
 
 				lightboxPhoto();
-						});		
-		$(this).addClass("active"); 			
+						});
+		$(this).addClass("active");
 		return false;
 	});
-	
+
 }//if quicksand
 
 });

099333ec0e8cc4ca24e78a28ed79ff833c9ccc84
页颂教育 <yesongedu@sina.com>
2019-11-11 10:24:47 +0800

    add system1-4

---
+++ routes/system22.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('system22', { title: '加盟系统' });
+});
+
+module.exports = router;

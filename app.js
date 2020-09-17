42bdc91022f46600bdc2386bee804403a575b402
页颂教育 <yesongedu@sina.com>
2020-09-17 18:08:06 +0800

    add 20091706

--- app.js
+++ app.js
@@ -182,6 +182,7 @@ app.use('/measurement', measurement);
 app.use('/result', result);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');

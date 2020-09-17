4fb9be00e2ec970008c1c4292f598ce2e9d28dc2
页颂教育 <yesongedu@sina.com>
2020-09-17 20:52:33 +0800

    add 20091709

---
+++ routes/new120091706.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('new120091706', { title: '新闻播报' });
+});
+
+module.exports = router;
